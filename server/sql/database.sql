bfef09df258ad0a470472bbd1b67a83dcb8ce8a9
Kitti Born <born.kitti97@gmail.com>
2023-07-06 18:28:36 +0200

    add queries for initializing tables and adding sample data

---
+++ server/sql/database.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS movie_booking;
+DROP TABLE IF EXISTS seats;
+DROP TABLE IF EXISTS halls;
+DROP TABLE IF EXISTS cinemas;
+
+CREATE TABLE IF NOT EXISTS cinemas (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS halls (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    row_amount INT NOT NULL,
+    column_amount INT NOT NULL,
+    cinema_id INT,
+    FOREIGN KEY (cinema_id) REFERENCES cinemas(id)
+);
+
+CREATE TABLE IF NOT EXISTS seats (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    position INT,
+    status ENUM('free', 'reserved', 'sold') DEFAULT 'free',
+    hall_id INT,
+    FOREIGN KEY (hall_id) REFERENCES halls(id)
+);
---
+++ server/sql/sampleData.sql
@@ -0,0 +1,7 @@
+INSERT INTO cinemas (name) VALUES ("Cinema A"), ("Cinema B"), ("Cinema C");
+
+INSERT INTO halls (name, row_amount, column_amount, cinema_id) 
+    VALUES 
+        ("Hall D", 1, 2, 1),
+        ("Hall E", 2, 3, 2),
+        ("Hall F", 3, 4, 3);
