dbd866e4ce2dca09b099f1cab5c05da32d5e9046
Kitti Born <born.kitti97@gmail.com>
2023-07-07 13:23:58 +0200

    refactor initDB, database is needed to be created beforehand unfortunately

--- server/src/sql/initDB.js
+++ server/src/sql/initDB.js
@@ -3,12 +3,12 @@ import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
-const DB_NAME = process.env.DB_NAME || "your_db";
 
 const db = mysql.createConnection({
     host: "localhost",
     user: process.env.DB_USER || "your_username",
     password: process.env.DB_PASSWORD || "your_password",
+    database: process.env.DB_NAME || "your_db",
     multipleStatements: true,
 });
 
@@ -24,42 +24,14 @@ db.connect((err) => {
     } else {
         console.info("Connected to mysql DB!");
 
-        // creating new DATABASE
-        db.query(
-            "CREATE DATABASE IF NOT EXISTS ??;",
-            DB_NAME,
-            (err, results) => {
-                if (err) console.info("Erro when creating Database: ", err);
-            }
-        );
-
-        // use newly created DATABASE
-        db.changeUser(
-            {
-                database: DB_NAME,
-            },
-            (err) => {
-                if (err) console.info("Error when Changing DB: ", err);
-            }
-        );
-
         db.query(createTables, (err, results) => {
-            if (err) console.info("Error when executing Query: ", err);
+            if (err) console.info("Error when creating Tables: ", err);
         });
 
         db.query(createRecords, (err, results) => {
-            if (err) console.info("Error when executing Query: ", err);
+            if (err) console.info("Error when creating Records: ", err);
         });
 
-        console.info("Database, Tables and Records Initiated!!");
-
-        // test query
-        db.query("Select * from cinemas;", (err, results) => {
-            if (err) {
-                console.log("Error when querying all cinemas: ", err);
-            } else {
-                console.log(results);
-            }
-        });
+        console.info("Tables and Records created successfully!");
     }
 });
