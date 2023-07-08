1d03ee2304cb1719e946c3a1bde8b5d2d54b77f0
Kitti Born <born.kitti97@gmail.com>
2023-07-08 22:08:28 +0200

    add more record for sample data

--- server/src/models/sql/sampleData.sql
+++ server/src/models/sql/sampleData.sql
@@ -4,8 +4,10 @@ INSERT INTO cinemas (name)
 
 INSERT INTO halls (name, row_amount, column_amount, cinema_id) 
     VALUES 
-        ("Hall A", 2, 3, 1),
-        ("Hall B", 3, 4, 2);
+        ("Hall AA", 2, 3, 1),
+        ("Hall AB", 3, 4, 1),
+        ("Hall BA", 4, 5, 2),
+        ("Hall BB", 5, 6, 2);
 
 
 INSERT INTO seats (position, status, hall_id) 
@@ -14,4 +16,8 @@ INSERT INTO seats (position, status, hall_id)
         (4, "reserved", 1),
         (6, "reserved", 2),
         (8, "sold", 2),
-        (9, "sold", 2);
+        (2, "sold", 3)
+        (3, "sold", 3)
+        (4, "sold", 3)
+        (12, "sold", 4),
+        (13, "sold", 4);
