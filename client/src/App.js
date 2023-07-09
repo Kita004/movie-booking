b8a092c615d9d6a6b79d0a5562a1fb9eaa435c19
Kitti Born <born.kitti97@gmail.com>
2023-07-09 13:50:38 +0200

    add socketContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { MainPage } from "./pages/MainPage";
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { MainPage } from "./pages/MainPage";
+import { CinemaDetail } from "./pages/CinemaDetail";
+import { HallDetail } from "./pages/HallDetail";
+import { CinemaCard } from "./components/CinemaCard";
+import { HallCard } from "./components/HallCard";
+
+import { socket, SocketContext } from "./context/socket";
+
 import {
     fetchCinemas,
     fetchHallsByCinemaId,
     fetchSeatsByHallId,
 } from "./utils/api";
 import { buildPositions } from "./utils/hallPositions";
-import { CinemaDetail } from "./pages/CinemaDetail";
-import { HallDetail } from "./pages/HallDetail";
-import { CinemaCard } from "./components/CinemaCard";
-import { HallCard } from "./components/HallCard";
 
 function App() {
     const [cinemas, setCinemas] = useState();
@@ -38,52 +42,54 @@ function App() {
     };
 
     return (
-        <div className="App flex-container">
-            <Router>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <MainPage>
-                                {cinemas
-                                    ? cinemas.map((cinema) => (
-                                          <CinemaCard
-                                              key={cinema.id}
-                                              id={cinema.id}
-                                              name={cinema.name}
-                                              getHalls={getHallsByCinemaId}
-                                          />
-                                      ))
-                                    : "Loading..."}
-                            </MainPage>
-                        }
-                    />
-                    <Route
-                        path="cinema/:id"
-                        element={
-                            <CinemaDetail>
-                                {halls
-                                    ? halls.map((hall) => (
-                                          <HallCard
-                                              key={hall.id}
-                                              id={hall.id}
-                                              name={hall.name}
-                                              rows={hall.row_amount}
-                                              cols={hall.column_amount}
-                                              getSeats={getSeatsByHallId}
-                                          />
-                                      ))
-                                    : "Loading..."}
-                            </CinemaDetail>
-                        }
-                    />
-                    <Route
-                        path="/hall/:id"
-                        element={<HallDetail seats={seats} />}
-                    />
-                </Routes>
-            </Router>
-        </div>
+        <SocketContext.Provider value={socket}>
+            <div className="App flex-container">
+                <Router>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <MainPage>
+                                    {cinemas
+                                        ? cinemas.map((cinema) => (
+                                              <CinemaCard
+                                                  key={cinema.id}
+                                                  id={cinema.id}
+                                                  name={cinema.name}
+                                                  getHalls={getHallsByCinemaId}
+                                              />
+                                          ))
+                                        : "Loading..."}
+                                </MainPage>
+                            }
+                        />
+                        <Route
+                            path="cinema/:id"
+                            element={
+                                <CinemaDetail>
+                                    {halls
+                                        ? halls.map((hall) => (
+                                              <HallCard
+                                                  key={hall.id}
+                                                  id={hall.id}
+                                                  name={hall.name}
+                                                  rows={hall.row_amount}
+                                                  cols={hall.column_amount}
+                                                  getSeats={getSeatsByHallId}
+                                              />
+                                          ))
+                                        : "Loading..."}
+                                </CinemaDetail>
+                            }
+                        />
+                        <Route
+                            path="/hall/:id"
+                            element={<HallDetail seats={seats} />}
+                        />
+                    </Routes>
+                </Router>
+            </div>
+        </SocketContext.Provider>
     );
 }
 
